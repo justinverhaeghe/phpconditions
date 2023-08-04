bb286ee6264acbd463b2276b4e2492939e549d6e
Forzalyonnais <verhaeghe.justin@yahoo.fr>
2023-08-04 10:06:38 +0200

    update conditions

---
+++ exo5/index.php
@@ -0,0 +1,55 @@
+<?php
+$gender = "Homme";
+?>
+
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Handjet:wght@300&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/assets/css/style.css">
+    <title>PHP Conditions - Exercice 5</title>
+</head>
+<!-- Header -->
+
+<body>
+    <header class="d-flex flex-column justify-content-center align-items-center text-light">
+        <h1>PHP Conditions</h1>
+        <h2>Exercice 5</h2>
+    </header>
+    <main>
+        <!-- Consigne de l'exercice -->
+        <div class=" text-center pt-4">
+            <p>Consigne : Traduire ce code avec des if et des else :
+
+                echo ($gender != 'Homme') ? 'C\'est une développeuse !!!' : 'C\'est un développeur !!!';.</p>
+
+        </div>
+        <hr>
+        <!-- Réponse -->
+        <div class="text-center ">
+            <p>
+                <?php
+                if ($gender == "Homme") {
+                    echo "C'est un développeur !!!";
+                } else {
+                    echo "C'est une développeuse !!!";
+                }
+                ?>
+
+            </p>
+        </div>
+
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ exo8/index.php
@@ -0,0 +1,55 @@
+<?php
+$isOk = true;
+?>
+
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Handjet:wght@300&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="./public/assets/css/style.css">
+    <title>PHP Conditions - Exercice 8</title>
+</head>
+<!-- Header -->
+
+<body>
+    <header class="d-flex flex-column justify-content-center align-items-center text-light">
+        <h1>PHP Conditions</h1>
+        <h2>Exercice 8</h2>
+    </header>
+    <main>
+        <!-- Consigne de l'exercice -->
+        <div class=" text-center pt-4">
+            <p>Consigne : Traduire ce code avec des if et des else :
+
+                echo ($isOk) ? 'c'est ok !!' : 'c'est pas bon !!!';</p>
+
+        </div>
+        <hr>
+        <!-- Réponse -->
+        <div class="text-center ">
+            <p>
+                <?php
+                if ($isOk) {
+                    echo "C'est ok";
+                } else {
+                    echo "C'est pas bon";
+                }
+                ?>
+
+            </p>
+        </div>
+
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
